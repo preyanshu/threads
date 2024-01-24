3b3722cfe2e38958a84e32146a4d6f6db8cd75a2
Preyanshu <mishrapreyanshu@gmail.com>
2024-01-24 18:21:47 +0530

    new file:   app/api/uploadthing/core.ts

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const page = () => {
+// import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+
+  // const userInfo = await fetchUser(user.id);
+  // if (userInfo?.onboarded) redirect("/");
+  const userInfo = {};
+  console.log("userInfo", user);
+
+
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image ||user?.imageUrl,
+  };
+  console.log("userdata",userData);
+
   return (
-    <div className='' style={{color:"white"}}>
-        i am onboarding
-      
-    </div>
-  )
+
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+      <h1 className='head-text'>Onboarding</h1>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
+      </p>
+
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
+    </main>
+  );
 }
 
-export default page
+export default Page;
---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,30 @@
+import { currentUser } from '@clerk/nextjs'
+// import { useRouter } from 'next/router'
+import { redirect } from 'next/navigation'
+// import { fetchUser } from 'pages/api/user/[id]'
+import { fetchUser } from '@/lib/actions/user.actions'
+import PostThread  from '@/components/forms/PostThread'
+
+
+
+import React from 'react'
+
+
+
+const page = async() => {
+    const user = await currentUser();
+    if(!user) return null
+
+    const  userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) return redirect('/onboarding');
+  return (
+    <>
+    <div className="head-text">
+        Create Thread
+    </div>
+    <PostThread  userId={userInfo._id}/>
+    </>
+  )
+}
+
+export default page
